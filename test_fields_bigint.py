edcb3d9b7dc80e2cc3ed0708bacaf05f65d4b903
Keryn Knight <keryn@kerynknight.com>
2015-04-11 18:11:25 +0100

    added more tests, this time around using a queryset to modify data and retrieve it

--- test_fields_bigint.py
+++ test_fields_bigint.py
@@ -51,9 +51,33 @@ def test_StrictBigIntegerField_ok():
 
 
 @pytest.mark.django_db
-def test_StrictBigIntegerField_ok():
+def test_StrictBigIntegerField_ok_until_changed():
+    """
+    Ensure this value is less than or equal to 15.
+    """
     model5 = models.BigIntegerFieldModel(field=15)
     assert model5.field == 15
     with pytest.raises(ValidationError):
         model5.field = 16
 
+
+@pytest.mark.django_db
+def test_StrictBigIntegerField_create_via_queryset():
+    """
+    Ensure this value is less than or equal to 15.
+    """
+    assert models.BigIntegerFieldModel.objects.count() == 0
+    with pytest.raises(ValidationError):
+        models.BigIntegerFieldModel.objects.create(field=16)
+    assert models.BigIntegerFieldModel.objects.count() == 0
+
+
+@pytest.mark.django_db
+def test_StrictBigIntegerField_update_via_queryset_invalid_then_get():
+    """
+    Ensure this value is less than or equal to 15.
+    """
+    model = models.BigIntegerFieldModel.objects.create(field=15)
+    model.__class__.objects.filter(pk=model.pk).update(field=16)
+    with pytest.raises(ValidationError):
+        model.__class__.objects.get(pk=model.pk)
