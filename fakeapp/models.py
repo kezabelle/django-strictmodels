bbd88936e7590c6370a23f75c108780c8275b769
Keryn Knight <keryn@kerynknight.com>
2015-04-11 14:07:46 +0100

    Here's a skeleton for a whole bunch of other fields. I don't know how many of them make sense, yet.

--- fakeapp/models.py
+++ fakeapp/models.py
@@ -13,3 +13,108 @@ class BigIntegerFieldModel(models.Model):
         validators.MinValueValidator(5),
         validators.MaxValueValidator(15)
     ])
+
+
+
+class BinaryFieldModel(models.Model):
+    field = models.BinaryField()
+
+
+
+class BooleanFieldModel(models.Model):
+    field = models.BooleanField()
+
+
+
+class CharFieldModel(models.Model):
+    field = models.CharField(max_length=255)
+
+
+
+class CommaSeparatedIntegerFieldModel(models.Model):
+    field = models.CommaSeparatedIntegerField(max_length=255)
+
+
+
+class DateFieldModel(models.Model):
+    field = models.DateField()
+
+
+
+class DateTimeFieldModel(models.Model):
+    field = models.DateTimeField()
+
+
+
+class DecimalFieldModel(models.Model):
+    field = models.DecimalField()
+
+
+
+class EmailFieldModel(models.Model):
+    field = models.EmailField()
+
+
+
+class FilePathFieldModel(models.Model):
+    field = models.FilePathField()
+
+
+
+class FloatFieldModel(models.Model):
+    field = models.FloatField()
+
+
+
+class GenericIPAddressFieldModel(models.Model):
+    field = models.GenericIPAddressField()
+
+
+
+class IPAddressFieldModel(models.Model):
+    field = models.IPAddressField()
+
+
+
+class IntegerFieldModel(models.Model):
+    field = models.IntegerField()
+
+
+
+class NullBooleanFieldModel(models.Model):
+    field = models.NullBooleanField()
+
+
+
+class PositiveIntegerFieldModel(models.Model):
+    field = models.PositiveIntegerField()
+
+
+
+class PositiveSmallIntegerFieldModel(models.Model):
+    field = models.PositiveSmallIntegerField()
+
+
+
+class SlugFieldModel(models.Model):
+    field = models.SlugField()
+
+
+
+class SmallIntegerFieldModel(models.Model):
+    field = models.SmallIntegerField()
+
+
+
+class TextFieldModel(models.Model):
+    field = models.TextField()
+
+
+
+class TimeFieldModel(models.Model):
+    field = models.TimeField()
+
+
+
+class URLFieldModel(models.Model):
+    field = models.URLField()
--- strictmodels.py
+++ strictmodels.py
@@ -28,3 +28,129 @@ class StrictBigIntegerField(fields.BigIntegerField):
     def contribute_to_class(self, cls, name, **kwargs):
         super(StrictBigIntegerField, self).contribute_to_class(cls, name, **kwargs)
         setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictBinaryField(fields.BinaryField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictBinaryField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictBooleanField(fields.BooleanField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictBooleanField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictCharField(fields.CharField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictCharField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictCommaSeparatedIntegerField(fields.CommaSeparatedIntegerField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictCommaSeparatedIntegerField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictDateField(fields.DateField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictDateField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictDateTimeField(fields.DateTimeField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictDateTimeField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictDecimalField(fields.DecimalField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictDecimalField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictEmailField(fields.EmailField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictEmailField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictFilePathField(fields.FilePathField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictFilePathField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictFloatField(fields.FloatField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictFloatField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictGenericIPAddressField(fields.GenericIPAddressField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictGenericIPAddressField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictIPAddressField(fields.IPAddressField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictIPAddressField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictIntegerField(fields.IntegerField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictIntegerField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictNullBooleanField(fields.NullBooleanField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictNullBooleanField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictPositiveIntegerField(fields.PositiveIntegerField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictPositiveIntegerField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictPositiveSmallIntegerField(fields.PositiveSmallIntegerField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictPositiveSmallIntegerField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictSlugField(fields.SlugField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictSlugField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictSmallIntegerField(fields.SmallIntegerField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictSmallIntegerField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictTextField(fields.TextField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictTextField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictTimeField(fields.TimeField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictTimeField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
+
+
+class StrictURLField(fields.URLField):
+    def contribute_to_class(self, cls, name, **kwargs):
+        super(StrictURLField, self).contribute_to_class(cls, name, **kwargs)
+        setattr(cls, self.name, FieldCleaningDescriptor(self))
